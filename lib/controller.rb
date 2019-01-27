0788b0c1d8e9f49ac4abe6199afa6d50d21207af
LordGoblin <maxime78600@gmail.com>
2019-01-27 01:59:14 +0100

    ok

--- lib/controller.rb
+++ lib/controller.rb
@@ -23,7 +23,6 @@ class Controller
 		puts "3 = (array_departement) == tout les departement"
 		puts "4 = (hachier_keydeppart_valuenumero) == hash |key = departement|value = numero|"
 		puts "5 = (hachier_invers_keynumero_valuedeppart) == hash |key = numero|value = departement"
-		puts "6 = (array_mairie_pour_https) == heuuuuu ca : (/59/lille.html) "
 		params = gets.chomp.to_i
 		case params
 		when 1
@@ -35,8 +34,6 @@ class Controller
 		when 4
 			@model.modedev(params)
 		when 5
-			@model.modedev(params)
-		when 6
 			@model.modedev(params)
 		else
 			puts "nop"
